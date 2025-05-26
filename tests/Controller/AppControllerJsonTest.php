34f5cc4e21b52f055437e96e6a374f22a0d4e5ff
Rebecka Corell <rebeckacorell@gmail.com>
2025-05-26 11:38:47 +0200

    Sc14

--- tests/Controller/AppControllerJsonTest.php
+++ tests/Controller/AppControllerJsonTest.php
@@ -18,6 +18,101 @@ class AppControllerJsonTest extends WebTestCase
         $client->catchExceptions(false);
         $client->request('GET', '/api/quote');
 
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test jsonDeck route.
+     */
+    public function testJsonDeck(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/api/deck');
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test jsonShuffle route.
+     */
+    public function testJsonShuffle(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/api/deck/shuffle');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test jsonDraw route.
+     */
+    public function testJsonDraw(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/api/deck/draw');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test jsonDrawMore route.
+     */
+    public function testJsonDrawMore(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/api/deck/draw/3');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test jsonDrawMore route.
+     */
+    public function testJsonDrawMoreMax(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/api/deck/draw/53');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test jsonGame route.
+     */
+    public function testJsonGame(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/api/game');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /api/library/books route.
+     */
+    public function testShowAllBooksApi(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/api/library/books');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /api/library/books/9780060188702 route.
+     */
+    public function testSearchByIsbnApi(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+
+        $client->request('GET', '/api/library/books/9780060188702');
 
         $this->assertResponseIsSuccessful();
     }
---
+++ tests/Controller/CardGameControllerTest.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Controller\CardGameController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CardGameControllerTest extends WebTestCase 
+{
+    /**
+     * Test /session and /session/delete route.
+     */
+    public function testSession(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/session');
+
+        $this->assertResponseIsSuccessful();
+
+        $client->request('GET', '/session/delete');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card route.
+     */
+    public function testCard(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/card');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card/deck route no session.
+     */
+    public function testDeckNoSession(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/card/deck');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card/deck route.
+     */
+    public function testDeck(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/session');
+        $client->request('GET', '/card/deck');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card/deck/shuffle route no session.
+     */
+    public function testShuffleNoSession(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/card/deck/shuffle');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card/deck/shuffle route.
+     */
+    public function testShuffle(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/session');
+        $client->request('GET', '/card/deck/shuffle');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card/deck/draw route no session.
+     */
+    public function testDrawNoSession(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/card/deck/draw');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card/deck/draw route.
+     */
+    public function testDraw(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/session');
+        $client->request('GET', '/card/deck/draw');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card/deck/draw{num} route no session.
+     */
+    public function testDrawManyNoSession(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/card/deck/draw/4');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Test /card/deck/draw/{num} route.
+     */
+    public function testDrawMany(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(false);
+        $client->request('GET', '/session');
+        $client->request('GET', '/card/deck/draw/50');
+        $client->request('GET', '/card/deck/draw/2');
+        $client->request('GET', '/card/deck/draw/3');
+
+        $this->assertResponseIsSuccessful();
+    }
+}
